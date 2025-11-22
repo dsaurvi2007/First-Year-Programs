c1641a5080715ad459f55f3c61a960ee74d5ba40
dsaurvi2007 <d.saurvi2007@gmail.com>
2025-11-22 23:40:26 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,20 @@
+package Study;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		System.out.println("*******   *******");
+		System.out.println("     $$   $$     ");
+		System.out.println("**    *****    **");
+		System.out.println("**    *****    **");
+		System.out.println("     $$   $$     ");
+		System.out.println("*******   *******");
+		System.out.println("*******   *******");
+
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
