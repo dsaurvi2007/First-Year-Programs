bfe72bdd91d4166bafe249a1494e09e098415859
dsaurvi2007 <d.saurvi2007@gmail.com>
2025-11-22 23:36:40 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package Study;
+
+public class Demo {//Sample program to print a statement
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
