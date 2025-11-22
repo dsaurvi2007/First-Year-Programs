7e7cd789e79b320f62c5d8205fb6c48ea1225358
dsaurvi2007 <d.saurvi2007@gmail.com>
2025-11-22 23:42:17 +0530

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,15 @@
+package Study;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Saurvi Tiwary");
+		System.out.println("Branch: Computer Science and Information Technology");
+		System.out.println("Year of Study: 1st Year");
+		System.out.println("University Roll no: 6929");
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
