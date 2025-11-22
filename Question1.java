ecbd4f14f09f33c27e589acfc49dba539ed58449
dsaurvi2007 <d.saurvi2007@gmail.com>
2025-11-22 23:37:26 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,14 @@
+package Study;
+
+public class Question1 {
+
+	public static void main(String[] args) {// To print the following statements
+		System.out.println("Hello \" ITERIAN \" ");
+		System.out.println("Siksha 'O' Anusandhan");
+		System.out.println("Welcome to \"Introduction to Computer Programming Lab\" ");
+		System.out.println("Java is fun for all");
+		// TODO Auto-generated method stub
+
+	}
+
+}
